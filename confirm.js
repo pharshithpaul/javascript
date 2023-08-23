74b69ea53b8e503966a97f05d630c13815447ba4
pharshithpaul <javanprakash123@gmail.com>
2023-08-23 13:40:05 +0530

    java

---
+++ confirm.js
@@ -0,0 +1,13 @@
+var name=prompt("Enter your name"," ");
+     if(name==="teja"){
+     	document.write("chhehehehehhe")
+     }else if(name==="swaroop"){
+     document.write("yadava")
+    }else if(name==="rohit"){
+     document.write("pranathi")
+    }else if(name==="rajesh"){
+     document.write("vibrator")
+    }
+    else{
+   	 document.write("data is invalid")
+   }
